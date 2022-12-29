e86590b273c02401e746e758a51364d2188e0437
julzhou6 <julzhou@DESKTOP-DK0A5DG>
2022-12-29 11:34:28 -0500

    added works page and more content to about page

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', (event) => {
+    new cursoreffects.fairyDustCursor({colors: ['#FFD700', '#FFFACD', '#FFFFF0', '#87CEEB']});
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,44 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/app.js',
-    mode: 'production',
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'bundle.js'
-    },
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.css$/, 
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ]
-            },
-            {
-                test: /\.(svg|gif|png|eot|woff|ttf)$/,
-                use: [
-                    'url-loader',
-                ],
-            }, 
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                }
-            }
-        ]
-    }, 
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-              }),
-            new CssMinimizerPlugin(),
-        ]
-    }
+  entry: "./src/js/app.js",
+  mode: "development",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "bundle.js",
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        use: ["url-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
 };
