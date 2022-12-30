fc5bd13b78ec57c712bc0dde4688bef0873354b0
julzhou6 <julzhou@DESKTOP-DK0A5DG>
2022-12-29 23:16:10 -0500

    bunny favicon

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,10 @@
-(()=>{var e={398:()=>{window.addEventListener("load",(function(e){new cursoreffects.fairyDustCursor({colors:["#FFD700","#FFFACD","#FFFFF0","#87CEEB"]})}))},691:(e,r,o)=>{"use strict";o.r(r)}},r={};function o(t){var n=r[t];if(void 0!==n)return n.exports;var i=r[t]={exports:{}};return e[t](i,i.exports,o),i.exports}o.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o(398),o(691)})();
+(()=>{var __webpack_modules__={"./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/(__unused_webpack_module,__unused_webpack_exports,__webpack_require__)=>{eval('__webpack_require__(/*! ./main.js */ "./src/js/main.js");\n__webpack_require__(/*! ../css/main.css */ "./src/css/main.css");\n\n//# sourceURL=webpack://julzhou6.github.io/./src/js/app.js?')},"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/()=>{eval("window.addEventListener('load', function (event) {\n  new cursoreffects.fairyDustCursor({\n    colors: ['#FFD700', '#FFFACD', '#FFFFF0', '#87CEEB']\n  });\n});\n\n//# sourceURL=webpack://julzhou6.github.io/./src/js/main.js?")},"./src/css/main.css":
+/*!**************************!*\
+  !*** ./src/css/main.css ***!
+  \**************************/(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://julzhou6.github.io/./src/css/main.css?")}},__webpack_module_cache__={};function __webpack_require__(_){var e=__webpack_module_cache__[_];if(void 0!==e)return e.exports;var s=__webpack_module_cache__[_]={exports:{}};return __webpack_modules__[_](s,s.exports,__webpack_require__),s.exports}__webpack_require__.r=_=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(_,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(_,"__esModule",{value:!0})};var __webpack_exports__=__webpack_require__("./src/js/app.js")})();
